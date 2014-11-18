9ced6aab7757039becb7e06bbbddf5dcce88615e
dennis84 <d.dietrich84@gmail.com>
2014-11-18 15:24:20 +0000

    PSR-4

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,5 @@
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $loader = require __DIR__.'/../vendor/autoload.php';
-$loader->add('Mapped\Tests', __DIR__ . '/../tests');
 
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
